2c4c4ec5fb9759905529c4da4e71175148a06ede
Reid Mason <maddogshain132@gmail.com>
2025-05-06 00:01:20 +0100

    refactor: broke up api response package

--- api/src/httpHandlers/models/apiResponse.go
+++ api/src/httpHandlers/models/apiResponse.go
@@ -18,9 +18,7 @@ func createErrorResponse(errors []string) apiResponse[any] {
 	return apiResponse[any]{Errors: errors}
 }
 
-func Ok[T any](w http.ResponseWriter, data T) {
-	w.WriteHeader(http.StatusOK)
-	response := createResponse(data)
+func writeResponse[T any](w http.ResponseWriter, response apiResponse[T]) {
 	responseJson, err := json.Marshal(response)
 	if err != nil {
 		InternalServerError[any](w, []string{"Failed to marshal response"})
@@ -28,49 +26,34 @@ func Ok[T any](w http.ResponseWriter, data T) {
 	}
 
 	w.Write(responseJson)
+}
+
+func Ok[T any](w http.ResponseWriter, data T) {
+	w.WriteHeader(http.StatusOK)
+	response := createResponse(data)
+	writeResponse(w, response)
 }
 
 func InternalServerError[T any](w http.ResponseWriter, errors []string) {
 	w.WriteHeader(http.StatusInternalServerError)
 	response := createErrorResponse(errors)
-	responseJson, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	w.Write(responseJson)
+	writeResponse(w, response)
 }
 
 func NotFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 	response := createErrorResponse([]string{"Resource not found"})
-	responseJson, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
-	w.Write(responseJson)
+	writeResponse(w, response)
 }
 
 func BadRequest[T any](w http.ResponseWriter, errors []string) {
 	w.WriteHeader(http.StatusBadRequest)
 	response := createErrorResponse(errors)
-	responseJson, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	w.Write(responseJson)
+	writeResponse(w, response)
 }
 
 func Created[T any](w http.ResponseWriter, data T) {
 	w.WriteHeader(http.StatusCreated)
 	response := createResponse(data)
-	responseJson, err := json.Marshal(response)
-	if err != nil {
-		InternalServerError[any](w, []string{"Failed to marshal response"})
-		return
-	}
-
-	w.Write(responseJson)
+	writeResponse(w, response)
 }
